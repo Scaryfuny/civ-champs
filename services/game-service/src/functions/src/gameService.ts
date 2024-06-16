f20d93d4fcaedfa7dbc6aad378b7a599888337e9
Maksym Bilohai <bmvscary@gmail.com>
2024-06-16 18:06:53 +0300

    Add game service Azure Function Api skeleton

---
+++ services/game-service/src/functions/src/gameService.ts
@@ -0,0 +1,9 @@
+
+
+export function getUserGames(): any[] {
+    return [
+        { name: 'Game 1', players: [{name : "Max"}, {name : "John"}] },
+        { name: 'Game 2', players: [{name : "Max"}, {name : "Brad"}] },
+        { name: 'Game 3', players: [{name : "Max"}, {name : "Luke"}] }
+    ];
+}
